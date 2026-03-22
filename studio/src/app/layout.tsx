d3ca5fed651f9809223a33bd0e691e9c304226df
mattisonchao <mattisonchao@gmail.com>
2026-03-22 23:37:43 +0800

    Fix Toaster: use sonner directly, remove next-themes dependency

--- studio/src/app/layout.tsx
+++ studio/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <main className="flex-1 overflow-y-auto p-4 md:p-6">
           {children}
         </main>
-        <Toaster theme="dark" richColors />
+        <Toaster />
       </body>
     </html>
   );
