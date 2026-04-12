c6f1e3c4047d8c1a402042157eaa2622e40c802e
mattisonchao <mattisonchao@gmail.com>
2026-04-12 16:37:29 +0800

    Replace env vars with Cobra flags, remove ext/env.go

--- pilot/cmd/pilot.go
+++ pilot/cmd/pilot.go
@@ -11,8 +11,7 @@ import (
 	slogzerolog "github.com/samber/slog-zerolog/v2"
 	"github.com/spf13/cobra"
 
-	"github.com/regret-io/regret/pilot-go/ext"
-	p "github.com/regret-io/regret/pilot-go"
+	pilot "github.com/regret-io/regret/pilot-go"
 )
 
 var rootCmd = &cobra.Command{
@@ -22,7 +21,10 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.Flags().String("data-dir", ext.EnvOr("DATA_DIR", "/data"), "Base data directory")
+	rootCmd.Flags().String("data-dir", "/data", "Base data directory")
+	rootCmd.Flags().Uint16("http-port", 8080, "HTTP server port")
+	rootCmd.Flags().String("namespace", "regret", "Kubernetes namespace")
+	rootCmd.Flags().String("auth-password", "", "Basic auth password for write operations (empty = disabled)")
 }
 
 func run(cmd *cobra.Command, _ []string) error {
@@ -30,26 +32,26 @@ func run(cmd *cobra.Command, _ []string) error {
 	slog.SetDefault(slog.New(slogzerolog.Option{Logger: &zerologLogger}.NewZerologHandler()))
 
 	dataDir, _ := cmd.Flags().GetString("data-dir")
-	httpPort := ext.EnvUint16("HTTP_PORT", 8080)
-	namespace := ext.EnvOr("NAMESPACE", "regret")
+	httpPort, _ := cmd.Flags().GetUint16("http-port")
+	namespace, _ := cmd.Flags().GetString("namespace")
+	authPw, _ := cmd.Flags().GetString("auth-password")
 
 	var authPassword *string
-	if pw := os.Getenv("AUTH_PASSWORD"); pw != "" {
-		authPassword = &pw
+	if authPw != "" {
+		authPassword = &authPw
 	}
 
 	slog.Info("starting regret-pilot", "data_dir", dataDir, "http_port", httpPort, "namespace", namespace)
 
-	pilot, err := p.New(dataDir, httpPort, namespace, authPassword)
+	p, err := pilot.New(dataDir, httpPort, namespace, authPassword)
 	if err != nil {
 		return err
 	}
 
-	// Context cancelled on SIGINT/SIGTERM
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	return pilot.Start(ctx)
+	return p.Start(ctx)
 }
 
 func main() {
